266f578671c36962f606445b4b4b0cdf93254dec
BrawlB3AST <mugge0645@gmail.com>
2026-08-04 16:36:22 +0800

    Update settings.py

--- CenturyInnovative/settings.py
+++ CenturyInnovative/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w=$vmerlr(v*6-hcz03f_om%co=7%4z@&tm7jlg=0v(bz8wnt&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['centuryinnovative2.onrender.com']
 
 
 # Application definition
